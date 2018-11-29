eb3f10f42caefd2ae20934f273bf188046bc5f27
Subhash Jha <subhashjha35@gmail.com>
2018-11-29 10:13:01 +0100

    change in push notification

--- src/app/shared/newsletter.service.ts
+++ src/app/shared/newsletter.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { KeysService } from './keys.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsletterService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private keys:KeysService) {
 
   }
 
   addPushSubscriber(sub:any) {
-      return this.http.post('/api/notifications', sub);
+    return this.http.post('http://prelay-api.herokuapp.com/v1/notification/?key='+this.keys.get_token(), sub);
   }
 
   send() {
-      return this.http.post('/api/newsletter', null);
+    return this.http.post('/api/newsletter', null);
   }
 }
